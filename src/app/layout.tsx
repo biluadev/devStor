d8bdc6bd7f29af270322f497cdb0c9c5a49bc127
biluadev <biluadevcod@gmail.com>
2024-02-12 18:16:16 +0100

    Estrutura da aplicação feita

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export const metadata: Metadata = {
-  title: "devstore",
-};
+  title: 'devstore',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.variable}>{children}</body>
+    <html lang="pt" className={inter.variable}>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
